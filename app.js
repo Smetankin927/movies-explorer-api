e7a03e69e516c7df9f99fad66ec6ec38af2c749a
Sergey Smetankin <smetankin.sd@phystech.edu>
2023-08-30 03:52:07 +0300

    localhost

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookies());
 app.use(bodyParser.json());
 
 // подключаемся к серверу mongo
-mongoose.connect("mongodb://localhost:27017/mestodb");
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 // подключаем мидлвары, роуты и всё остальное...
 app.use(requestLogger); // подключаем логгер запросов
 //краш-тест
